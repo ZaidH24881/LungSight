9c6147bf60db175b7629748cca0cd1f715331008
msalim5 <ashazsalim7@gmail.com>
2025-11-01 12:40:54 -0500

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "4rem" }}>
       <h1>Welcome to Hackathon X-Ray </h1>
       <p>Your AI-powered medical imaging app starts here.</p>
+      <button> Upload X-Ray</button>
     </div>
   );
 }
